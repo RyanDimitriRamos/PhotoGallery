57bdc80d4005c4521c71197af412b25502d18f7b
Ryan Dimitri Ramos <dimitriramos99@gmail.com>
2020-10-12 19:03:52 -0700

    Implemented a rudimentary notification for when new photos are fetched from flickr. This is done using a worker object. I will clean this up in a little bit.

--- app/src/main/java/com/dimitriusramos/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/dimitriusramos/android/photogallery/QueryPreferences.kt
@@ -6,6 +6,8 @@ import android.preference.PreferenceManager
 import androidx.core.content.edit
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
+private const val PREF_LAST_RESULT_ID = "lastResultId"
+private const val PREF_IS_POLLING = "isPolling"
 
 object QueryPreferences {
 
@@ -16,4 +18,26 @@ object QueryPreferences {
 
     fun setStoredQuery(context: Context, query: String) =
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString(PREF_SEARCH_QUERY, query).apply()
+
+    fun getLastResultId(context: Context): String {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(PREF_LAST_RESULT_ID, "")!!
+    }
+
+    fun setLastResultId(context: Context, lastResultId: String) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putString(PREF_LAST_RESULT_ID, lastResultId)
+        }
+    }
+
+    fun isPolling(context: Context): Boolean {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean(PREF_IS_POLLING, false)
+    }
+
+    fun setPolling(context: Context, isOn: Boolean) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putBoolean(PREF_IS_POLLING, isOn)
+        }
+    }
 }
